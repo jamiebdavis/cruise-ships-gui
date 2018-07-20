c2c4c1545a07ab656b0433174956b913b298d14b
Jamie Davis <jamied1985@gmail.com>
2018-07-20 12:25:08 +0100

    completed step 3

---
+++ src/controller.js
@@ -0,0 +1,22 @@
+function Controller() {
+    this.initialiseSea();
+};
+
+Controller.prototype = {
+    initialiseSea() {
+        const backgrounds = [
+            "./images/water0.png",
+            "./images/water1.png"
+        ];
+        let backgroundIndex = 0;
+        setInterval(() => {
+            const viewport = document.querySelector("#viewport");
+            viewport.style.backgroundImage = `url("${backgrounds[backroundIndex]}")`
+
+            backgroundIndex += 1;
+            if (backgroundIndex === backgrounds.length) {
+                backgroundIndex = 0;
+            }
+        }, 1000);
+    }
+}
