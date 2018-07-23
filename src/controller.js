496b4f98fa25a63e57ed0a794c2ce6177fb9aa7f
Jamie Davis <jamied1985@gmail.com>
2018-07-23 12:11:25 +0100

    step 5 changes

--- src/controller.js
+++ src/controller.js
@@ -20,8 +20,29 @@
                     backgroundIndex = 0;
                 }
             }, 1000);
+        },
+        renderPorts (ports) {
+            const portElement = document.querySelector("#ports")
+            portElement.style.width = "0px";
+
         }
-    }
+    };
+    ports.forEach((port, index) => {
+        const newPortElement = document.createElement("div");
+        newPortElement.className = "port";
+
+        newPortElement.dataset.portIndex = index; 
+
+        portsElement.appendChild(newPortElement);
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`
+
+    });
+
+
+
+
+
     if (typeof module !== "undefined" && module.exports) {
         module.exports = Controller;
     } else {
